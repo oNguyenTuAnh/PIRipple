86a8cadd250385390ee07b837cf399ed7391cc0e
nguyen.tu.anh <nguyen.tu.anh@framgia.com>
2018-06-21 09:29:53 +0700

    Update swift 3

--- PIRipple/PIRipple/DemoView.swift
+++ PIRipple/PIRipple/DemoView.swift
@@ -3,14 +3,14 @@ import UIKit
 
 class DemoView1 : UIView {
   
-  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    super.touchesBegan(touches, withEvent: event)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
     for touch: AnyObject in touches {
       let t: UITouch = touch as! UITouch
-      let location = t.locationInView(self)
+        let location = t.location(in: self)
       
       //RIPPLE BORDER
-      rippleBorder(location, color: UIColor.whiteColor())
+        rippleBorder(location: location, color: UIColor.white)
 
     }
   }
@@ -19,14 +19,14 @@ class DemoView1 : UIView {
 
 class DemoView2 : UIView {
   
-  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    super.touchesBegan(touches, withEvent: event)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
     for touch: AnyObject in touches {
       let t: UITouch = touch as! UITouch
-      let location = t.locationInView(self)
+        let location = t.location(in: self)
 
       //RIPPLE FILL
-      rippleFill(location, color: UIColor.whiteColor())
+        rippleFill(location: location, color: UIColor.white)
 
     }
   }
